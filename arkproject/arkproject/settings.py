0a6fefd7a3032e0907daf8e477b45d9bbad84c74
hirwacedric123 <122902659+hirwacedric123@users.noreply.github.com>
2023-03-25 13:49:08 +0200

    Update settings.py

--- arkproject/arkproject/settings.py
+++ arkproject/arkproject/settings.py
@@ -76,15 +76,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'arkproject.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+#  Database
+#  https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+ DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 
 
 # Password validation
